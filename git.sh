80d533649703742412b49783bdb6c5751d3f6c56
thang9139 <hiimmichaelqs@gmail.com>
2025-02-16 08:16:21 +0700

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,1 +1,1 @@
-git clone https:// ghp_pz2qq7ay8AICoA441tB48Cwc1sltpm2rTnOH@github.com /thang9139/thang.git
+git clone https://ghp_pz3qq7ay8AICoA441tB48Cwc1sltpm2rTnOF@github.com/thang9139/thang.git
