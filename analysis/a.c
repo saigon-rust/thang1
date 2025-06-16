30e47455f22e4db31b483d94384ec2c1d9f85b47
thang9139 <hiimmichaelqs@gmail.com>
2025-06-16 21:44:56 +0700

    Thang thay doi

---
+++ analysis/a.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+typedef enum { RED, BLACK } PieceColor;
+typedef enum { EMPTY, RED_ROOK, BLACK_ROOK, ... } PieceType;
+
+typedef struct {
+    PieceType type;
+    PieceColor color;
+} Piece;
+
+    double advertising_cost[] = {10, 15, 20, 25, 30, 35, 40, 45, 50, 55};
+    double sales[] = {120, 150, 180, 200, 230, 250, 280, 300, 330, 350};
+
+int main() {
+
+
+    return 0;
+}
